bc3a0fc2a6125784f71ea66f1ecd14eb59bd47a6
Kim Truong <35829229+slickw1lly@users.noreply.github.com>
2018-10-24 07:48:04 -0700

    Initial commit.

--- cgi-bin/cgi_1.py
+++ cgi-bin/cgi_1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import cgi
-
+import cgitb
+cgitb.enable()
+1/0
 
 cgi.test()
--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -6,4 +6,7 @@ cgitb.enable()
 
 print("Content-type: text/plain")
 print()
-print("Your job is to make this work")
+form = cgi.FieldStorage()
+listval = form.getlist("operand")
+total = sum(map(float, listval))
+print(f"Total is {total}")
